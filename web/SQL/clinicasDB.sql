095bf235d5104e6a38d49f9661642af3f866eb33
Sergio López <60165858+lopezsergio29@users.noreply.github.com>
2020-04-13 17:47:33 -0600

    Hice cambio en la tabla de clientes por redundancia

--- web/SQL/clinicasDB.sql
+++ web/SQL/clinicasDB.sql
@@ -10,26 +10,21 @@
 
 CREATE SCHEMA `clinicasdb` ;
 
-use clinicasdb;
-CREATE TABLE `clinicasdb`.`usuarios` (
-  `idUsuario` INT NOT NULL AUTO_INCREMENT,
-  `usuario` VARCHAR(145) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `tipo` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`idUsuario`));
-
 CREATE TABLE `clinicasdb`.`doctores` (
   `idDoctor` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(145) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
   `nombres` VARCHAR(145) NOT NULL,
   `apellidos` VARCHAR(145) NOT NULL,
   `celular` VARCHAR(9) NOT NULL,
   `correo` VARCHAR(145) NOT NULL,
   `credencial` VARCHAR(145) NOT NULL,
-  `idUsuario` INT NOT NULL,
   PRIMARY KEY (`idDoctor`));
 
 CREATE TABLE `clinicasdb`.`pacientes` (
   `idPaciente` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(145) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
   `nombres` VARCHAR(145) NOT NULL,
   `apellidos` VARCHAR(145) NOT NULL,
   `foto` LONGBLOB,
@@ -40,24 +35,13 @@ CREATE TABLE `clinicasdb`.`pacientes` (
   `correo` VARCHAR(145) NOT NULL,
   `sexo` TINYINT(1) NOT NULL,
   `seguro` TINYINT(1) NOT NULL,
-  `idDoctor` INT NOT NULL,
-  `idUsuario` INT NOT NULL,
-  PRIMARY KEY (`idPaciente`));
-
-CREATE TABLE `clinicasdb`.`cuadroMedico` (
-  `idCuadroMedico` INT NOT NULL AUTO_INCREMENT,
   `estatura` DOUBLE NOT NULL,
   `tipoSangre` VARCHAR(3) NOT NULL,
   `alergias` VARCHAR(200) NOT NULL,
   `historialFamiliar` VARCHAR(200) NOT NULL,
-  `idPaciente` INT NOT NULL,
-PRIMARY KEY (`idCuadroMedico`));
+  `idDoctor` INT,
+  PRIMARY KEY (`idPaciente`));
 
-CREATE TABLE `clinicasdb`.`enfermedades` (
-  `idEnfermedad` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(145) NOT NULL,
-  `idPaciente` INT NOT NULL,
-  PRIMARY KEY (`idEnfermedad`));
 
 CREATE TABLE `clinicasdb`.`citas` (
   `idCita` INT NOT NULL AUTO_INCREMENT,
@@ -66,42 +50,6 @@ CREATE TABLE `clinicasdb`.`citas` (
   `idPaciente` INT NOT NULL,
   `idDoctor` INT NOT NULL,
   PRIMARY KEY (`idCita`));
-
-ALTER TABLE `clinicasdb`.`pacientes` 
-ADD INDEX `fk_usuarioP_idx` (`idUsuario` ASC) VISIBLE,
-ADD INDEX `fk_doctorP_idx` (`idDoctor` ASC) VISIBLE;
-;
-ALTER TABLE `clinicasdb`.`pacientes` 
-ADD CONSTRAINT `fk_usuarioP`
-  FOREIGN KEY (`idUsuario`)
-  REFERENCES `clinicasdb`.`usuarios` (`idUsuario`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_doctorP`
-  FOREIGN KEY (`idDoctor`)
-  REFERENCES `clinicasdb`.`doctores` (`idDoctor`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `clinicasdb`.`doctores` 
-ADD INDEX `fk_usuarioD_idx` (`idUsuario` ASC) VISIBLE;
-;
-ALTER TABLE `clinicasdb`.`doctores` 
-ADD CONSTRAINT `fk_usuarioD`
-  FOREIGN KEY (`idUsuario`)
-  REFERENCES `clinicasdb`.`usuarios` (`idUsuario`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `clinicasdb`.`enfermedades` 
-ADD INDEX `fk_pacienteE_idx` (`idPaciente` ASC) VISIBLE;
-;
-ALTER TABLE `clinicasdb`.`enfermedades` 
-ADD CONSTRAINT `fk_pacienteE`
-  FOREIGN KEY (`idPaciente`)
-  REFERENCES `clinicasdb`.`pacientes` (`idPaciente`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
 ALTER TABLE `clinicasdb`.`citas` 
 ADD INDEX `fk_doctorC_idx` (`idDoctor` ASC) VISIBLE,
@@ -118,13 +66,13 @@ ADD CONSTRAINT `fk_pacienteC`
   REFERENCES `clinicasdb`.`pacientes` (`idPaciente`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
-
-ALTER TABLE `clinicasdb`.`cuadromedico` 
-ADD INDEX `fk_pacienteCM_idx` (`idPaciente` ASC) VISIBLE;
+  
+ALTER TABLE `clinicasdb`.`pacientes` 
+ADD INDEX `fk_pacienteD_idx` (`idDoctor` ASC) VISIBLE;
 ;
-ALTER TABLE `clinicasdb`.`cuadromedico` 
-ADD CONSTRAINT `fk_pacienteCM`
-  FOREIGN KEY (`idPaciente`)
-  REFERENCES `clinicasdb`.`pacientes` (`idPaciente`)
+ALTER TABLE `clinicasdb`.`pacientes` 
+ADD CONSTRAINT `fk_pacienteD`
+  FOREIGN KEY (`idDoctor`)
+  REFERENCES `clinicasdb`.`doctores` (`idDoctor`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
