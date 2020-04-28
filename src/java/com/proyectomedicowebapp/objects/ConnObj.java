428fe9906f6fe66850b33a3ee94070a84e05dde1
SaraValentina <valentina.galindo20>
2020-04-28 11:58:02 -0600

    Fotos asistente2 y doctora2

--- src/java/com/proyectomedicowebapp/objects/ConnObj.java
+++ src/java/com/proyectomedicowebapp/objects/ConnObj.java
@@ -2,7 +2,7 @@ package com.proyectomedicowebapp.objects;
 
 public class ConnObj {
     public String getConn() {
-        String connString = "jdbc:mysql://localhost/clinicasdb?user=root&password=12345&autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String connString = "jdbc:mysql://localhost/clinicasdb?user=root&password=SanJorge20&autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         
         return connString;
     }
